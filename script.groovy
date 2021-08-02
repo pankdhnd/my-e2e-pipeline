21368c4f4b84b2a7ddd4645d79002fb35c268b22
Pankaj Dhande <pank.dhnd@gmail.com>
2021-08-02 08:36:44 +0530

    Corrected directory name

--- script.groovy
+++ script.groovy
@@ -1,7 +1,7 @@
 def buildTar(){
     echo "Building application tar file"
     sh 'mkdir app'
-    sh 'cp -R public views app.js package.json /app'
+    sh 'cp -R public views app.js package.json app'
     sh 'tar -czvf app.tar.gz app'
 }//buildTar
 
