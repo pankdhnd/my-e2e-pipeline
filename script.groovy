13276debf2847242e13ba2dc60fca833f8c8343f
Pankaj Dhande <pank.dhnd@gmail.com>
2021-08-02 08:39:10 +0530

    Updated

--- script.groovy
+++ script.groovy
@@ -1,6 +1,6 @@
 def buildTar(){
     echo "Building application tar file"
-    sh 'rm -f app'
+    sh 'rm -r app'
     sh 'mkdir app'
     sh 'cp -R public views app.js package.json app'
     sh 'tar -czvf app.tar.gz app'
