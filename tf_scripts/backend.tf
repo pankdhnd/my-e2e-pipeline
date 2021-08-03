937d8bed506a1b6d3dda1021ea7cd19b00465870
Pankaj Dhande <pank.dhnd@gmail.com>
2021-08-03 08:06:40 +0530

    updated

--- tf_scripts/backend.tf
+++ tf_scripts/backend.tf
@@ -1,10 +1,10 @@
 terraform{
     backend "s3" {
+      profile = "test"
+      shared_credentials_file = "~/.aws/credentials"
       bucket = "pipeline1-remote-backend"
       key = "terraform.tfstate"
       region = "ap-south-1"
-      dynamodb_table = "app-s3-tf-state-lock"
-      profile = "test"
-      shared_credentials_file = "~/.aws/credentials"
+      dynamodb_table = "app-s3-tf-state-lock"      
     }    
 }
