5a41fdb8d1c2647dc2c9286971e7c0e3b0549836
Pankaj Dhande <pank.dhnd@gmail.com>
2021-08-04 00:32:06 +0530

    Updated shell script

--- copyKey.sh
+++ copyKey.sh
@@ -5,7 +5,7 @@ NUMBER_OF_PARAMETERS=${#}
 if [[ NUMBER_OF_PARAMETERS -lt 1 ]]
 then
 	echo "Insufficient parameters. Usage: $(basename ${0}) <Key String>"
-    exit 1
+  exit 1
 fi
 
 
@@ -15,7 +15,10 @@ KEY_FILE_NAME=ec2key.pem
 #Check if ssh directory exists. If yes, then delete the directory
 if [[ ! -d $DIR ]]
 then
+  echo "Directory ~/.ssh not found, creating now"
   mkdir $DIR
+else
+  echo "Directory ~/.ssh already exists, skipped create operation"
 fi
 
 #Create key if it does not exist
@@ -24,5 +27,7 @@ then
     KEY=${1}
     #copy inputkey to .ssh location
     cp ${1} ~/.ssh/$KEY_FILE_NAME
+else
+    echo "Key file already exists, skipped copy operation"
 fi
 
